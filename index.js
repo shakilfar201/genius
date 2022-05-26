d5ac8d825b9508d8621c19734e6050a9f1c65289
shakilfar201 <shakilfarhan15@gmail.com>
2022-05-26 11:23:17 +0600

    service

--- index.js
+++ index.js
@@ -105,7 +105,7 @@ async function run() {
         app.put('/booking', async (req, res) => {
             const user = req.body;
             const filter = { token : user.token };
-            const updateDoc = { $set: { status: 'confirm', taka: user.taka }  };
+            const updateDoc = { $set: { status: 'confirm', taka: user.taka, service: user.service }};
             const result = await bookingCollection.updateOne(filter, updateDoc );
             res.json(result);
         });
