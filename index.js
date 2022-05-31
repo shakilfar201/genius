a4b59f756f8d63a5bab814901b0270417184c6ad
shakilfar201 <shakilfarhan15@gmail.com>
2022-05-31 17:08:05 +0600

    ccc

--- index.js
+++ index.js
@@ -38,14 +38,6 @@ async function run() {
             res.json(result)
         });
 
-        // app.get('/invoice/:id', async (req, res) => {
-        //     const id = req.params.id;
-        //     // console.log(id);
-        //     const booking = { _id: ObjectId(id) };
-        //     const result = bookingCollection.findOne(booking)
-        //     res.json(result)
-        // });
-
         // get all car
         app.get('/carmodel', async (req, res) => {
             const cursor = carmodelCollection.find({})
@@ -89,6 +81,14 @@ async function run() {
             // console.log(id)
             const product = { _id: ObjectId(id) };
             const result = await productsCollection.findOne(product);
+            res.json(result)
+        });
+
+        app.get('/booking/:id', async (req, res) => {
+            const id = req.params.id;
+            // console.log(id)
+            const product = { _id: ObjectId(id) };
+            const result = await bookingCollection.findOne(product);
             res.json(result)
         });
 
