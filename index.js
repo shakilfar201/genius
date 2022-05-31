f3b2d9807d2e09758c85faf526d2f322a2ff50a1
shakilfar201 <shakilfarhan15@gmail.com>
2022-05-31 13:36:30 +0600

    aa

--- index.js
+++ index.js
@@ -38,8 +38,9 @@ async function run() {
             res.json(result)
         });
 
-        app.get('/booking/:id', async (req, res) => {
+        app.get('/invoice/:id', async (req, res) => {
             const id = req.params.id;
+            // console.log(id);
             const booking = { _id: ObjectId(id) };
             const result = bookingCollection.findOne(booking)
             res.json(result)
