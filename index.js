e4e8c26a84ecde179bdc5cac1489d6270e213ba3
shakilfar201 <shakilfarhan15@gmail.com>
2022-06-07 17:10:07 +0600

    date

--- index.js
+++ index.js
@@ -115,7 +115,7 @@ async function run() {
             var orderDate = {
                 date: {
                     $gte: new Date(date.form).toLocaleDateString(),
-                    $lte: new Date(date.to).toLocaleDateString()
+                    $lt: new Date(date.to).toLocaleDateString()
                 }
             }
             const cursor = bookingCollection.find(orderDate);
